bf497059be4221d4fe61ba528e9e830a7e36d394
Poum <poum@cpan.org>
2012-11-25 16:19:26 +0100

    add legal header to each file

--- js/app/locale/fr.js
+++ js/app/locale/fr.js
@@ -1,10 +1,23 @@
 /**
- * France (France) translation
- * By Poum 
- * 08-11-2012, 10:39 AM
- * updated by xxx (22 Sep 2008)
- * updated by yyy (20 Apr 2010)
- */
+French message
+
+Copyright 2012 Philippe Poumaroux
+
+This file is part of cucumber-json2report.
+
+cucumber-json2report is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 Ext.define('CJ2H.locale.fr', {
 	extend: 'CJ2H.locale.default',
 
